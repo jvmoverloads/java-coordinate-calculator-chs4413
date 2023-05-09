c20dd476fa8b8c12d7e00a1d2f250afd3c57384b
채희승 <chs4413@gmail.com>
2023-05-10 01:27:03 +0900

    feat,test: 도메인 객체 구현 및 각 도메인 생성 테스트(new-branch)

--- src/main/java/coordinate/domain/Point.java
+++ src/main/java/coordinate/domain/Point.java
@@ -1,53 +1,27 @@
 package coordinate.domain;
 
 public class Point {
-    private final int x;
-    private final int y;
-
-    private static final int MIN_VALUE = 0;
-    private static final int MAX_VALUE = 24;
+    private final Coordinate x;
+    private final Coordinate y;
 
     public Point(int x, int y) {
-        checkRange(x, y);
-        this.x = x;
-        this.y = y;
+        this.x = new Coordinate(x);
+        this.y = new Coordinate(y);
     }
 
-    private void checkRange(int x, int y) {
-        if (x < MIN_VALUE || x > MAX_VALUE)
-            throw new IllegalArgumentException("잘못된 좌표 값");
-        if (y < MIN_VALUE || y > MAX_VALUE)
-            throw new IllegalArgumentException("잘못된 좌표 값");
-    }
-
-    // 피타고라스 정리 a^2 + b^2 = c^2(빗변)
-    // ex. (2, 4), (5, 8)
-    // -> x축 차이(길이): 3(5-2)
-    // -> y축 차이(길이): 4(8-4)
-    // -> 3^2 + 4^2 = x^2 -> 9 + 16 = 25(5^2)
-    // answer: 5
     public double getInstance(Point point) {
         int x = point.getX();
         int y = point.getY();
 
-        return Math.sqrt(Math.pow((x - this.x), 2) + Math.pow((y - this.y), 2));
+        return Math.sqrt(Math.pow((x - this.x.getCoordinate()), 2) + Math.pow((y - this.y.getCoordinate()), 2));
     }
 
     public int getX() {
-        return x;
+        return x.getCoordinate();
     }
 
     public int getY() {
-        return y;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + x;
-        result = prime * result + y;
-        return result;
+        return y.getCoordinate();
     }
 
     @Override
@@ -59,10 +33,25 @@ public class Point {
         if (getClass() != obj.getClass())
             return false;
         Point other = (Point) obj;
-        if (x != other.x)
+        if (x == null) {
+            if (other.x != null)
+                return false;
+        } else if (!x.equals(other.x))
             return false;
-        if (y != other.y)
+        if (y == null) {
+            if (other.y != null)
+                return false;
+        } else if (!y.equals(other.y))
             return false;
         return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((x == null) ? 0 : x.hashCode());
+        result = prime * result + ((y == null) ? 0 : y.hashCode());
+        return result;
     }
 }
--- src/test/java/fuel/RentCompanyTest.java
+++ src/test/java/fuel/RentCompanyTest.java
@@ -1,3 +1,5 @@
+package fuel;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
